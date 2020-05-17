ec45517b19a62cc583ad456186fd3facac73db0b
Andrew Yourtchenko <ayourtch@gmail.com>
2020-05-17 17:18:32 +0000

    Execute each of the command line arguments

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ fn main() {
     let ts_netops_host = std::env::var("TS_NETOPS_HOST").unwrap();
     let ts_netops_user = std::env::var("TS_NETOPS_USER").unwrap();
     let ts_netops_pass = std::env::var("TS_NETOPS_PASS").unwrap();
-    let command_to_run = std::env::args().nth(1).unwrap();
 
     let tcp_target = format!("{}:23", ts_netops_host);
 
@@ -127,44 +126,46 @@ fn main() {
         }
     }
 
-    connection
-        .write(&format!("{}\n", command_to_run).as_bytes())
-        .unwrap();
-    login_state = LoginState::ReadingOutput;
-    let mut result = "".to_string();
+    for command_to_run in std::env::args().skip(1) {
+        connection
+            .write(&format!("{}\n", command_to_run).as_bytes())
+            .unwrap();
+        login_state = LoginState::ReadingOutput;
+        let mut result = "".to_string();
 
-    loop {
-        use telnet::TelnetEvent;
-        let event = connection.read().expect("Read Error");
-        match &event {
-            TelnetEvent::Data(bytes) => {
-                let string = String::from_utf8_lossy(&bytes);
-                data_buffer.push_str(&string);
-                match login_state {
-                    LoginState::ReadingOutput => {
-                        let maybe_privexec_match = privexec_regex.find(&data_buffer);
-                        if let Some(privexec_match) = maybe_privexec_match {
-                            debug!("Matched privexec prompt! Data buffer: {}", &data_buffer);
-                            let (command_output, remainder) =
-                                data_buffer.split_at(privexec_match.end());
-                            result = command_output.to_string();
-                            data_buffer = remainder.to_string();
-                            login_state = LoginState::Established;
-                            break;
+        loop {
+            use telnet::TelnetEvent;
+            let event = connection.read().expect("Read Error");
+            match &event {
+                TelnetEvent::Data(bytes) => {
+                    let string = String::from_utf8_lossy(&bytes);
+                    data_buffer.push_str(&string);
+                    match login_state {
+                        LoginState::ReadingOutput => {
+                            let maybe_privexec_match = privexec_regex.find(&data_buffer);
+                            if let Some(privexec_match) = maybe_privexec_match {
+                                debug!("Matched privexec prompt! Data buffer: {}", &data_buffer);
+                                let (command_output, remainder) =
+                                    data_buffer.split_at(privexec_match.end());
+                                result = command_output.to_string();
+                                data_buffer = remainder.to_string();
+                                login_state = LoginState::Established;
+                                break;
+                            }
+                        }
+                        _ => {
+                            debug!(
+                                "Other state: {:?}. data buffer: {}",
+                                &login_state, &data_buffer
+                            );
                         }
                     }
-                    _ => {
-                        debug!(
-                            "Other state: {:?}. data buffer: {}",
-                            &login_state, &data_buffer
-                        );
-                    }
+                }
+                _ => {
+                    debug!("{:?}", &event);
                 }
             }
-            _ => {
-                debug!("{:?}", &event);
-            }
         }
+        println!("Result: {}", &result);
     }
-    println!("Result: {}", &result);
 }
