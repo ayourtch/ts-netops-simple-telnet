6525b16ed3b63d2aa58225dd419a95ea7c8c3977
Andrew Yourtchenko <ayourtch@gmail.com>
2020-05-17 13:10:47 +0000

    Add first telnet code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate telnet;
+use telnet::Telnet;
+
 fn main() {
-    println!("Hello, world!");
+    let ts_netops_host = std::env::var("TS_NETOPS_HOST").unwrap();
+    let tcp_target = format!("{}:23", ts_netops_host);
+
+    let mut connection =
+        Telnet::connect(tcp_target, 256).expect("Couldn't connect to the server...");
+    loop {
+        let event = connection.read().expect("Read Error");
+        println!("{:?}", event);
+    }
 }
